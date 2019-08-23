642ad8ed31397694f1f4f9edaad5f183d7c161d7
Joshua Hill <josh@jlhworkshop.com>
2019-08-23 20:38:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,11 @@ class Application
   
   def initialize
     t = Time.now.hour
-    if t 
+    if t <= 12
+      puts "Good Morning!"
+    else
+      puts "Good Afternoon!"
+    end
   end
   
 end
